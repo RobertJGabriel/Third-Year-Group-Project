b5fa2924d543791f05a471cb8508e4bdbc8cb8ee
MikeMahoney <mikemahoney0492@gmail.com>
2014-11-04 19:40:21 +0000

    Responsive Header with Menu

--- assests/view/profile/index.php
+++ assests/view/profile/index.php
@@ -9,8 +9,19 @@
 
 
 <div id="wrapperProfile">
-
-
+	<img id="mobileLogo" src="assests/img/gplogo2.png" width="150" height="50">
+		<div id="mobileButton">
+			<div id="mobileMenu">
+				<ul>
+					<li><a href="index.php?profile=true"><img src="<?php  echo 'assests/img/profilePhotos/' . $_SESSION['id'] . '.png' ?>"  width="60" height="60" alt="Profile Picture"></a></li>
+					<li><?php echo $_SESSION["fname"] ;  ?></li>
+					<li><a href="index.php?settings=true">Settings</a></li>
+					<li><a href="index.php?workout=true">Workout</a></li>
+					<li><a href="index.php?booking=true">Book </a></li>
+					<li><a href="index.php?logout=true"> Logout</a></li>
+				</ul>
+			</div>
+		</div>
     <div id="leftProfile">
         <ul id="profileMenu">
           <li><a href="index.php?profile=true"><img src="<?php  echo 'assests/img/profilePhotos/' . $_SESSION['id'] . '.png' ?>"  width="150" height="150" alt="Profile Picture"></a></li>
