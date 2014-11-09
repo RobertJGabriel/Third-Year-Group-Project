bf35488e7729e173661d8ba6af3c05d5d5621336
RobertGabeiel <RobertGabriel@Roberts-iMac.local>
2014-11-09 20:57:07 +0000

    Added Johns Improved css for the layout

--- assests/view/booking/index.php
+++ assests/view/booking/index.php
@@ -1,56 +1,62 @@
 
 <div class="header" id="profileHeader">
-    <div id="nav">
-
-
-        <ul id="menu">
-            <li id="menuButton"></li>
-            <li class="menuItem">
-                <a href="index.php?profile=true"><img src="<?php  echo 'assests/img/profilePhotos/' . $_SESSION['id'] . '.png' ?>"  width="90" height="90" alt="Profile Picture"></a>
-            </li>
-
-            <li class="menuItem">
-                <a href="index.php?profile=true">Home</a>
-            </li>
-            <li class="menuItem">
-                <a href="index.php?workout=true">Workout</a>
-            </li>
-            <li class="menuItem">
-                <a href="index.php?booking=true">Book </a>
-            </li>
-
-            <?php
-            if ($_SESSION["status"] == 3)
-            {
-                ?>
-
-                <li class="menuItem">
-                    <a href="index.php?timetable=true">Time Table</a>
-                </li>
-                <li class="menuItem">
-                    <a href="index.php?admin=true">Admin</a>
-                </li>
-
-            <?php
-            }
-
-
-            ?>
-            <li class="menuItem">
-                <a href="index.php?settings=true">Settings</a>
-            </li>
-            <li class="menuItem">
-                <a href="index.php?logout=true"> Logout</a>
-            </li>
+    <div id="socialPicks">
+        <ul>
+            <li id="twitter"><a href="https://twitter.com/CIT_Gym" target="_blank"></a></li>
+            <li id="facebook"><a href="https://www.facebook.com/CITGym" target="_blank"></a></li>
         </ul>
+    </div>
+
+    <div id="topLogo">
+        <div id="logo">
+            <img src="assests/img/logo.jpg" alt="Cit Logo">
+        </div>
+    </div>
+
+    <div id="logOutBox">
+        <div class="logOutButton"><a class="logOutLink" href="index.php?logout=true"> Logout</a></div>
     </div>
 </div>
 
 
 
 
+
 <div id="wrapperProfile">
 
+
+
+
+    <div id="leftProfile">
+        <ul id="profileMenu">
+            <li id="profilePic"><a href="index.php?profile=true"><img src="<?php  echo 'assests/img/profilePhotos/' . $_SESSION['id'] . '.png' ?>"  width="150" height="150" alt="Profile Picture"></a></li>
+            <li id="usersName"><h1><?php echo $_SESSION["fname"] ;  ?></h1></li>
+            <li class="profileButton"><a href="index.php?settings=true">Settings</a></li>
+            <li class="profileButton"><a href="index.php?workout=true">Workout</a></li>
+
+
+            <?php
+            if ($_SESSION["status"] == 3)
+            {
+                ?>
+
+                <li class="profileButton"><a href="index.php?admin=true">Admin </a></li>
+                <li class="profileButton"><a href="index.php?timetable=true">Schedule</a></li>
+
+            <?php
+            }
+
+
+            ?>
+            <li class="profileButton"><a href="index.php?booking=true">Book </a></li>
+            <li class="profileButton"><a href="index.php?logout=true">Logout</a></li>
+        </ul>
+    </div>
+
+
+
+
+    <div id="rightProfile">
 
 
     <ul id="booking">
@@ -86,6 +92,10 @@
     </ul>
 
 
+    </div>
+
+
+
 </div>
 
 
@@ -99,7 +109,7 @@
 
 
 
-            <script src="assests/javascript/jqueryui/external/jquery/jquery.js"></script>
+<script src="assests/javascript/jqueryui/external/jquery/jquery.js"></script>
             <script src="assests/javascript/jqueryui/jquery-ui.min.js"></script>
             <script src="assests/javascript/calendarScript.js"></script>
 
