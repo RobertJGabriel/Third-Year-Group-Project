286d65ecbb4653d545fa1730facafd8ba451d336
RobertJGabriel <robert_gabriel@outlook.com>
2014-12-10 21:01:04 +0000

    Added  Mobile meta
    <meta name="viewport" content="width=device-width,
    height=device-height, initial-scale=1" />
    
    also renamed titles on page

--- backup/assests/view/homepage/header.php
+++ backup/assests/view/homepage/header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Gym System : By Project bird </title>
+    <title>Gym System : By CIT </title>
+          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="assests/css/style.css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src = "assests/javascript/javascript.js"></script>
--- backup/assests/view/profile/header.php
+++ backup/assests/view/profile/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Profile</title>
+          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="assests/css/style.css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src = "assests/javascript/javascript.js"></script>
