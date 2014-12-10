36bfc5349779cb2c364cb3ede3ce8c9b230ccfaf
RobertJGabriel <robert_gabriel@outlook.com>
2014-12-10 11:08:39 +0000

    Added View Header for mobile website.

--- assests/view/settings/header.php
+++ assests/view/settings/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Settings</title>
-    
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
         <link rel="stylesheet" href="assests/css/CheatSheet/animations.css">
     
     <link rel='stylesheet' media='screen and (min-width: 640px) and (max-width: 9000px)' href='assests/css/normal/style.css' />
